736e9d4b4a9c0e0370132cf429bbe8ae38d82656
corey888773 <piotrgasiorek773@gmail.com>
2026-05-27 23:25:27 +0200

    create trackers structs

---
+++ crates/meta/src/trackers.rs
@@ -0,0 +1,96 @@
+use bencoding::{Value, from_json};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Debug, Serialize)]
+pub struct Request {
+    pub info_hash: String,
+    pub peer_id: String,
+    pub ip: Option<String>,
+    pub port: i64,
+    pub uploaded: i64,
+    pub downloaded: i64,
+    pub left: i64,
+    pub event: String,
+}
+
+impl From<&Request> for Value {
+    fn from(value: &Request) -> Self {
+        let json: serde_json::Value =
+            serde_json::to_value(value).expect("serialization shouldn't fail");
+        from_json(json)
+    }
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+pub struct Peer {
+    pub peer_id: String,
+    pub ip: Option<String>,
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+pub struct SuccessRespone {
+    pub interval: i64,
+    pub peers: Vec<Peer>,
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+pub struct FailureResponse {
+    pub failure_reason: String,
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+pub enum Response {
+    Success(SuccessRespone),
+    Failure(FailureResponse),
+}
+
+impl From<&Response> for Value {
+    fn from(value: &Response) -> Self {
+        let json: serde_json::Value =
+            serde_json::to_value(value).expect("serialization shouldn't fail");
+        from_json(json)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn happypath_bencoded_request() -> Result<(), Box<dyn std::error::Error>> {
+        let _ = Value::from(&Request {
+            info_hash: String::from(""),
+            peer_id: String::from(""),
+            ip: Some(String::from("")),
+            port: 0,
+            uploaded: 0,
+            downloaded: 0,
+            left: 0,
+            event: String::from(""),
+        });
+
+        Ok(())
+    }
+
+    #[test]
+    fn happypath_bencoded_success_response() -> Result<(), Box<dyn std::error::Error>> {
+        let _ = Value::from(&Response::Success(SuccessRespone {
+            interval: 0,
+            peers: vec![Peer {
+                peer_id: String::from(""),
+                ip: Some(String::from("")),
+            }],
+        }));
+
+        Ok(())
+    }
+
+    #[test]
+    fn happypath_bencoded_failure_response() -> Result<(), Box<dyn std::error::Error>> {
+        let _ = Value::from(&Response::Failure(FailureResponse {
+            failure_reason: String::from(""),
+        }));
+
+        Ok(())
+    }
+}
